0e68b99622a719152321df9da0af475897ac3601
Prateek Srivastava <prateek.srivastava@gmail.com>
2024-09-13 21:38:58 -0400

    Custom CSS file support.

--- swagger.go
+++ swagger.go
@@ -212,9 +212,9 @@ const indexTemplate = `<!-- HTML for static distribution bundle build -->
   <meta charset="UTF-8">
   <title>Swagger UI</title>
   <link rel="stylesheet" type="text/css" href="./swagger-ui.css" >
-  {% if .CustomCSS|length > 1 %}
+  {{ if .CustomCSS }}
   <link rel="stylesheet" type="text/css" href="{{.CustomCSS}}.css" >
-  {% endif %}
+  {{ end }}
   <link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="./favicon-16x16.png" sizes="16x16" />
   <style>
